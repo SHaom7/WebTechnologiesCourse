b856048a1ce6263d87c7749cfce6498471988034
shdn <shdn.ab.alomar@gmail.com>
2025-02-17 08:59:45 +0300

    lab 3 (tasks 1, 2, 3, 4, 5)

--- django/libraryproject/apps/bookmodule/views.py
+++ django/libraryproject/apps/bookmodule/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+#def index(request):
+#   return HttpResponse("Hello, world!")
+
+
+def index1(request):
+    name = request.GET.get("name") or "world!"
+    return HttpResponse("Hello, "+name)
+ 
+def index2(request, val1 = 0):   
+    return HttpResponse("value1 = "+str(val1))
+
+#def index(request): 
+    #name = request.GET.get("name") or "world!"
+    #return render(request, "bookmodule/index.html")    
+
+ 
+def index(request):
+    name = request.GET.get("name") or "world!"
+    return render(request, "bookmodule/index.html" , {"name": name})  
--- django/libraryproject/libraryproject/urls.py
+++ django/libraryproject/libraryproject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import apps.bookmodule.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/', apps.bookmodule.views.index),
+    path('', apps.bookmodule.views.index1),
+    path('index2/<int:val1>/', apps.bookmodule.views.index2)    
+  
 ]
