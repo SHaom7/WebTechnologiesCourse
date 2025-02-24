d1d0a09fb99afdf4aa73dc3ab3f77d422263ff54
shdn <shdn.ab.alomar@gmail.com>
2025-02-24 08:48:02 +0300

    lab 4 (tasks 1, 2, 3)

--- django/libraryproject/apps/bookmodule/urls.py
+++ django/libraryproject/apps/bookmodule/urls.py
@@ -2,11 +2,9 @@ from django.urls import path, include
 from apps.bookmodule import views
 
 urlpatterns = [
-    #path('first/', views.index),
-    #path('', views.index1),
-    #path('index2/<int:val1>/', views.index2) 
-    path('', views.index),
-    path('index2/<int:val1>/', views.index2),
-    path('<int:bookId>', views.viewbook)
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
 
